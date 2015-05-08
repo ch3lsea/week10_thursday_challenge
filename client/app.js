4dafd4afd2604afc33353e4ef5c4b05cfd8c7739
Chelsea Okey <chelsea.okey@gmail.com>
2015-05-08 09:18:35 -0500

    Adds Bootstrap and jQuery using Grunt

---
+++ client/app.js
@@ -0,0 +1,16 @@
+$('#tabz a[href="#home"]').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show')
+});
+$('#tabz a[href="#tab1"]').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show')
+});
+$('#tabz a[href="#tab2"]').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show')
+});
+$('#tabz a[href="#tab3"]').click(function (e) {
+    e.preventDefault();
+    $(this).tab('show')
+});
--- server/app.js
+++ server/app.js
@@ -3,15 +3,14 @@ var app = express();
 var path = require('path');
 var favicon = require('serve-favicon');
 
-app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
-//app.use(favicon(__dirname + '/public/images/favicon.ico'));
-
 app.set('port', (process.env.PORT || 5000));
 
 app.get('/*', function(request, response) {
     var file = request.params[0] || 'views/index.html';
     response.sendFile(path.join(__dirname, './public', file));
 });
+
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 app.listen(app.get('port'), function() {
     console.log("App is running on port: ", app.get('port'));
